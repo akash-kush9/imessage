2f61fd2e50433e93030c5bc77065fec5f25cb004
akash-kush9 <akash.kushwaha9@gmail.com>
2020-10-14 18:03:47 +0530

    Chat component created

--- src/IMessage/Chat/Chat.js
+++ src/IMessage/Chat/Chat.js
@@ -1,37 +1,64 @@
 import { IconButton } from '@material-ui/core'
-import { Message, MicOutlined } from '@material-ui/icons'
-import React, { useState } from 'react'
+import {  MicOutlined } from '@material-ui/icons'
+import React, { useState,useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectChannel } from '../../features/channelSlice'
+import { selectUser } from '../../features/userSlice'
+import db from '../../firebase'
+import firebase from 'firebase'
+import Message from './Message/Message'
 import './Chat.css'
 const Chat = () => {
+    const dispatch = useDispatch();
+    const user = useSelector(selectUser)
+    const channel = useSelector(selectChannel)
     const [message,setMessage] = useState('')
+    const [messages,setMessages] = useState([])
     const sendMessage = (e) => {
         e.preventDefault();
-
-        console.log(message)
+        // lastMessage :
+        db.collection('channels').doc(channel?.channelId).collection('messages').add({
+             message : message,timestamp:firebase.firestore.FieldValue.serverTimestamp(),
+             user : user.email , username:user.displayName
+        })
         setMessage('')
     }
+    useEffect(() => {
+        if (channel.channelId){
+            db.collection('channels').doc(channel?.channelId).collection('messages').orderBy('timestamp',"desc").onSnapshot(snapshot =>{
+                setMessages(snapshot.docs.map(doc=>({
+                    id:doc.id,
+                    ...doc.data()
+                })))
+            })
+        }
+        return () => {
+        }
+    }, [channel,dispatch])
     return (
-        <div className='chat'>
+
+        <div className='chat' key={channel?.channelId}>
             <div className='chat__header'>
-                <h4>To: <span className='chat__name'> Channel Name </span></h4>
+    <h4>To: <span className='chat__name'> {channel?.channelName}</span></h4>
                 <strong>Details</strong>
             </div>
-            <div className='chat__body'>
-                ChatBody
-                ChatBody
-            </div>
-            <div className='chat__input'>
-                <form >
-                    <input placeholder='iMessage' onChange={(e)=>{setMessage(e.target.value)}}  value={message} type='text' />
-                    <button onClick={sendMessage} >Send Message</button>
-                </form>
-                <IconButton>
-                    <MicOutlined/>
-                </IconButton>
-            </div>
-
-        </div>
-    )
-}
-
-export default Chat
+            <div className='chat__messages'>
+                {
+                    messages.map(messageDetails =>(
+                    <Message messageDetails={} /> ))}
+                    <div className='chat__input'>
+                        <form >
+                            <input placeholder='iMessage' onChange={(e)=>{setMessage(e.target.value)}}  value={message} type='text' />
+                            <button onClick={sendMessage} >Send Message</button>
+                        </form>
+                        <IconButton>
+                            <MicOutlined/>
+                        </IconButton>
+                    </div>
+        
+                </div>
+            )
+        }
+        
+        export default Chat
+        
