80ff39f570b76f839979c4691ca9729a83790d8c
akash-kush9 <akash.kushwaha9@gmail.com>
2020-10-14 18:00:39 +0530

    Login component created with google authentications

---
+++ src/Login/Login.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Login.css'
+import {Button} from '@material-ui/core'
+import {auth,googleProvider} from './../firebase'
+const Login = () => {
+    const signIn = () => {
+        auth.signInWithPopup(googleProvider).then(authuser=>
+            console.log(authuser)).catch(error => alert(error.message))
+    }
+    return (
+        <div className='login'>
+            <div className='login__logo'>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/IMessage_logo_%28Apple_Inc.%29.png/250px-IMessage_logo_%28Apple_Inc.%29.png'  alt='Imessage logo'/>
+                <h1>iMessage</h1>
+            </div>
+            <Button onClick={signIn}>
+            SignIn with Google
+            </Button> 
+        </div>
+    )
+}
+
+export default Login
