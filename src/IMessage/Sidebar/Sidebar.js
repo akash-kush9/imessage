7dcac3b2f666e04c3f86fe25dec0abe1d5a16320
akash-kush9 <akash.kushwaha9@gmail.com>
2020-10-14 19:22:59 +0530

    File restored

--- src/IMessage/Sidebar/Sidebar.js
+++ src/IMessage/Sidebar/Sidebar.js
@@ -1,10 +1,50 @@
- result.id ,
+import React from 'react'
+import './Sidebar.css'
+import {Avatar, IconButton} from '@material-ui/core'
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import SearchIcon from '@material-ui/icons/Search';
+import SidbarChat from './SidebarChat/SidebarChat'
+import {selectUser} from './../../features/userSlice'
+import {setChannel,selectChannel} from './../../features/channelSlice'
+
+import db from './../../firebase';
+import firebase from 'firebase';
+const Sidebar = () => {
+    const user = useSelector(selectUser)
+    const channel = useSelector(selectChannel)
+   
+    /*   
+     const user = {
+        displayName : 'Akash ',
+        photoURL : 'https://i.pinimg.com/736x/f3/43/d6/f343d6bc14838938d75c0d970f872247.jpg' 
+    } 
+ 
+                    result.id ,
                     channelName : channelInput,
                 }))
             ).catch(error => console.log(error))
         }
     }
-    
+    */
+   const handleClick = (id,channelName) =>{
+    dispatch(setChannel({
+        channelId : id ,
+        channelName : channelName, 
+    }))
+   }
+    const createChat = () => {
+        const input = alert('Please enter a chat room name')
+        db.collection('channels').add({
+            channelName:input,
+            timestamp : firebase.firestore.FieldValue.serverTimestamp(),
+            lastMessage:''
+        }).then(result => {
+            dispatch(setChannel({
+                channelId : result.id ,
+                channelName : input,
+            }))
+        }).catch(error => console.log(error))
+    }
     useEffect(() => {
         // Set all chat records for sidebar 
         db.collection('channels').onSnapshot(snapshot=>
