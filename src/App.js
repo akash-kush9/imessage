99fe508e9dce5b848ae46d27344204d3fd18d0a8
akash-kush9 <akash.kushwaha9@gmail.com>
2020-10-14 18:01:16 +0530

    Case for i message or login page landing implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import { useSelector,useDispatch } from 'react-redux';
 import './App.css';
+import { selectUser,login,logout } from './features/userSlice';
 import IMessage from './IMessage/IMessage';
-
+import Login from './Login/Login';
+import {auth} from './firebase';
+import { selectChannel } from './features/channelSlice';
 function App() {
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+  useEffect(() => {
+     auth.onAuthStateChanged(authuser =>{
+    authuser ?  dispatch(login({
+          uid : authuser.uid,
+          displayName : authuser.displayName,
+          email : authuser.email,
+          photoURL : authuser.photoURL
+      })): dispatch(logout() )
+
+    })
+    return () => {
+    }
+  }, [dispatch])
   return (
-    <div className='app'> <IMessage/></div>
+    <div className='app'> {
+    user ?( <IMessage/>) :( <Login />)
+    
+    }</div>
   );
 }
 
