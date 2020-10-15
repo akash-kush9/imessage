eafda126f2649cd5d2f69474afcc45a093b5abaf
akash-kush9 <akash.kushwaha9.com>
2020-10-15 14:10:07 +0530

    FlipMove configuration added to the component

--- src/IMessage/Chat/Message/Message.js
+++ src/IMessage/Chat/Message/Message.js
@@ -1,6 +1,30 @@
-import React from 'react'
+import { Avatar } from '@material-ui/core'
+import React ,{forwardRef}from 'react'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../../../features/userSlice'
+import * as timeago from 'timeago.js'
 import './Message.css'
-const Message = (messageDetails) => {
-    // console.log(message,id,timestamp,user,username)
-    return 
-    <h1>message</h1>
+const Message = forwardRef( ({messageDetails,id} , ref) => {
+    const user = useSelector(selectUser)
+    return (
+        <div ref={ref} className={`message ${user.email === messageDetails.email && 'message__sender'}  `} id={id} key={id}>
+            <Avatar className='message__avatar' src={messageDetails?.photoURL}>
+            </Avatar>
+            <div className='messageInfo'>
+                <p>
+                    {messageDetails?.message} 
+                </p>
+                   <small className='message__timestamp'>
+                       {
+                            messageDetails.timestamp ? 
+                            timeago.format(new Date(messageDetails.timestamp?.toDate()).toLocaleString()):  '' 
+                       }
+                      </small> 
+            </div>
+           
+ 
+        </div>
+    )
+})
+
+export default Message
